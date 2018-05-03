23c048899dd018aba10ae21b108ef9575563e956
Domenik Irrgang <domenikirrgang@web.de>
2018-05-03 15:47:54 +0200

    Init

---
+++ ragefirechasm/bosses/Adarogg.lua
@@ -0,0 +1,12 @@
+local adaroggModule = RageFireModule:newModule("Adarogg", 1443)
+local darkshamanModule = RageFireModule:newModule("Dark Shaman Koranthal", 1444)
+local darkshamanModule = RageFireModule:newModule("Lava Guard Gordoth", 1446)
+local darkshamanModule = RageFireModule:newModule("Slagmaw", 1445)
+
+function adaroggModule:init()
+    self:addGameEvent("COMBAT_LOG_EVENT_UNFILTERED")
+end
+
+function adaroggModule:COMBAT_LOG_EVENT_UNFILTERED()
+
+end
---
+++ ragefirechasm/ragefirechasm.lua
@@ -0,0 +1,9 @@
+RageFireModule = SuuBossMods.dungeonModuleLoader:newModule("Ragefire Chasm", 389)
+
+function RageFireModule:init() 
+
+end
+
+function RageFireModule:unload()
+   
+end
