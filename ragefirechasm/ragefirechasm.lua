2a9450930cda8e66b11856787e9faf9151da4ffb
Domenik Irrgang <domenikirrgang@web.de>
2018-05-08 00:11:22 +0200

    Example Eventcallback

--- ragefirechasm/ragefirechasm.lua
+++ ragefirechasm/ragefirechasm.lua
@@ -1,9 +1,18 @@
 RageFireModule = SuuBossMods.dungeonModuleLoader:newModule("Ragefire Chasm", 389)
 
 function RageFireModule:init() 
-
+    self.combatLogEventDispatcher:addEventCallback("SPELL_CAST_START", 119420, RageFireModule.startCast)
+    self.combatLogEventDispatcher:addEventCallback("SPELL_CAST_START", 119420, RageFireModule.stopCast)
 end
 
 function RageFireModule:unload()
    
+end
+
+function RageFireModule:startCast(combatLogEvent) 
+    print("Casting " .. combatLogEvent.spellName .. "...")
+end
+
+function RageFireModule:stopCast(combatLogEvent) 
+    print("Cast " .. combatLogEvent.spellName .. " ended")
 end
